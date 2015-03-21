bb37fabfed3fb1fb43599ee1bbd50079be223337
SagarAkshayan <sagar.akshayan@gmail.com>
2015-03-21 20:48:25 +0530

    created maven project

---
+++ example-app/src/main/java/com/edurek/co/ExcelReading.java
@@ -0,0 +1,10 @@
+package com.edurek.co;
+
+import org.apache.poi.hssf.usermodel.HSSFWorkbook;
+
+public class ExcelReading {
+
+	public static void main(String[] args) {
+		HSSFWorkbook workdboool ;
+	}
+}
